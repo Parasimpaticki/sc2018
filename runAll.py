90eb2f02b4c4c37f5a94baa8f79b6e3b628c835e
Parasimpaticki <stefanbogdanovic996@gmail.com>
2019-02-06 23:59:00 +0100

    Added new line to out.txt output

--- runAll.py
+++ runAll.py
@@ -4,6 +4,6 @@ import os
 f=open("out.txt","w+")
 for filename in os.listdir('data'):
 	rezultat = scproj.start("data/"+str(filename))
-	f.write(str(filename)+"\t"+str(rezultat))
+	f.write(str(filename)+"\t"+str(rezultat)+"\n")
 f.close()
 
